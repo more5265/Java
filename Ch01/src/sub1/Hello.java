ee8e80f3e74492ce603a47d013dac9eee28ea2e3
more5265 <more5265@gmail.com>
2023-06-13 15:21:46 +0900

    Ch01 수업예제

---
+++ Ch01/src/sub1/Hello.java
@@ -0,0 +1,38 @@
+package sub1;
+
+/**
+ * 날짜 : 2023/06/12
+ * 이름 : 이황성
+ * 내용 : Ch01.Java 개발환경 구축
+ */
+public class Hello {
+	
+	// 프로그램 진입점
+	public static void main(String[] args) {
+		
+		// 기본출력
+		System.out.println("Hello World~");
+		System.out.println("Hello Java~");
+		
+		
+		// 한줄출력
+		System.out.print("Hello ");
+		System.out.print("Korea~");
+		
+		// 개행처리
+		System.out.print("\n");
+		System.out.print("Hello Busan~");
+		System.out.print("\n");
+		
+		//println 및 print 활용
+		int x = 10;
+		int y = 20;
+		
+		System.out.println("x의 값은:" + x);
+		System.out.println("y의 값은:" + y);
+		
+		System.out.print("x + y의 값은: ");
+		System.out.print(x + y);
+
+	}
+}
