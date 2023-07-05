1f5ecd352938c0bedd748d39617b5f4c6eb24f40
more5265 <more5265@gmail.com>
2023-07-05 17:42:38 +0900

    복습 업데이트

---
+++ HsFreeTest/src/ch16/sec01/Calculable.java
@@ -0,0 +1,8 @@
+package ch16.sec01;
+
+@FunctionalInterface
+public interface Calculable {
+	
+	void calculate(int x, int y);
+
+}
---
+++ HsFreeTest/src/ch16/sec03/LambdaExample.java
@@ -0,0 +1,5 @@
+package ch16.sec03;
+
+public class LambdaExample {
+
+}
