cd162460a1f4bdbe742031129541d84b4274311b
more5265 <more5265@gmail.com>
2023-07-21 17:46:31 +0900

    7/21 자바 업데이트

--- CodingTest/src/step3/Test02.java
+++ CodingTest/src/step3/Test02.java
@@ -8,14 +8,17 @@ public class Test02 {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		int a = sc.nextInt();
-
-		int b = sc.nextInt();
+		int T = sc.nextInt();
 		
+		for(int i = 0 ; i < T; i++){
+			System.out.print("테스트 케이스 " + (i + 1) + " 입력 (A B): ");
+			int a = sc.nextInt();
+
+			int b = sc.nextInt();
+			
 		int sum = a + b;
-		System.out.println("합 : " + sum);
-		
-		sc.close();
-	}
-
+        System.out.println("테스트 케이스 " + (i + 1) + ": " + sum);
+		}
+sc.close();
+}
 }
